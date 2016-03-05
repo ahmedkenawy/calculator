c9ccf792a48fb7aa9a66205f627d3606eb43c47f
ahmedkenawy <ahmedabdoknawe@ymail.com>
2016-03-05 08:59:15 +0200

    add num val

--- assist/js/main.js
+++ assist/js/main.js
@@ -2,3 +2,34 @@
   $("#op1").on("click", function(event){
      $(".screen").val("");
  });
+ //add add val to screen
+ $("#num1").on("click", function(event){
+    $(".screen").val("1");
+});
+$("#num2").on("click", function(event){
+   $(".screen").val("2");
+});
+$("#num3").on("click", function(event){
+   $(".screen").val("3");
+});
+$("#num4").on("click", function(event){
+   $(".screen").val("4");
+});
+$("#num5").on("click", function(event){
+   $(".screen").val("5");
+});
+$("#num6").on("click", function(event){
+   $(".screen").val("6");
+});
+$("#num7").on("click", function(event){
+   $(".screen").val("7");
+});
+$("#num8").on("click", function(event){
+   $(".screen").val("8");
+});
+$("#num9").on("click", function(event){
+   $(".screen").input().val("9");
+});
+$("#num0").on("click", function(event){
+   $(".screen").val("0");
+});
